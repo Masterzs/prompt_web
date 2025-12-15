d89ea2a5e178b93a2255646dfc9a1febf727865c
Masterzs <46218645+Masterzs@users.noreply.github.com>
2025-12-15 10:24:46 +0800

    图片

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,3 +1,14 @@
+/// <reference types="vite/client" />
+
 interface Window {
   gtag?: (command: string, action: string, params?: any) => void;
+}
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string
+  // 可以添加其他环境变量类型
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
